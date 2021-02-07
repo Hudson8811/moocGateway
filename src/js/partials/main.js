0bc08a0d997fe193942d2fb97836ecae309773ec
hudson8811 <hudson88@yandex.ru>
2021-02-07 20:08:40 +0300

    chanegd

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -10,3 +10,21 @@ $('.mobile-overlay').click(function(e){
     $('.mobile-overlay').removeClass('mobile-overlay--active');
 });
 
+$('.profile-menu-link').click(function(e){
+    e.preventDefault()
+    const $this = $(this);
+    const id = $this.data('id');
+
+    if(!$this.hasClass('profile-menu-link--active')){
+        $('.profile-menu-link').removeClass('profile-menu-link--active');
+        $this.addClass('profile-menu-link--active');
+        $('.profile-mobile-wrapper').removeClass('profile-mobile-wrapper--active');
+        $(`.profile-mobile-wrapper[data-id="${id}"]`).addClass('profile-mobile-wrapper--active');
+    }
+});
+
+$('.bac-admin-btn').click(function(e){
+    e.preventDefault();
+    $('.profile-mobile-wrapper').removeClass('profile-mobile-wrapper--active');
+    $('.profile-menu-link').removeClass('profile-menu-link--active');
+})
